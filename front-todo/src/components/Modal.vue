c6c72b0a662de285bbd1ba0e44351b6994e596c9
KeisukeMaenaka <k-maenaka@pro-vision.jp>
2024-08-06 13:29:31 +0900

    httpリクエストをaxiosからfetchに変更

---
+++ front-todo/src/components/Modal.vue
@@ -0,0 +1,49 @@
+<script setup>
+    const emit = defineEmits(['closeModal']);
+
+    const stopEvent = (e) => {
+        e.stopPropagation();
+    }
+    
+    const test = () => {
+        console.log("aaa");
+    }
+</script>
+
+<template>
+    <div id="overlay">
+        <div id="content" @click="stopEvent">
+            <p>これがモーダルウィンドウです。</p>
+            <p><button @click="emit('closeModal')">close</button></p>
+            <button @click="test">テスト</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #overlay {
+        /* 要素を重ねたときの順番 */
+        z-index: 1;
+
+        /* 画面全体を覆う設定 */
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color:rgba(66, 64, 64, 0.411);
+
+        /* 画面の中央に要素を表示させる設定 */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    #content {
+        z-index: 2;
+        width: 50%;
+        padding: 1em;
+        background: #fff;
+    }
+
+</style>
--- front-todo/src/components/home.vue
+++ front-todo/src/components/home.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
     import axios from 'axios';
     import { ref, onMounted } from 'vue';
+    import Modal from './Modal.vue';
 
     const text = ref('');
+    const deadline = ref();
     const tasks = ref();
+    const status = ref();
+    const showContent = ref(false);
+
+    const openModal = () => {
+        console.log('click');
+        showContent.value = true;
+    };
+
+    const closeModal = () => {
+        showContent.value = false;
+    };
+    
 
     const getTasks = async () => {
         await axios.get("http://localhost:3000/api/tasks")
@@ -15,30 +29,114 @@
     }
     onMounted(async() => {
         await getTasks();
-    })
+    });
 
-    const postTasks = async () => {
-        // console.log(text);
-        await axios.post("http://localhost:3000/api/tasks", {task: text}
-        ).then(res => {
-            tasks.value = res.data;
-        }).catch(err => {
-            console.log(err);
-        })
-        await getTasks();
+    // const postTask = async () => {
+    //     // console.log(text);
+    //     await axios.post("http://localhost:3000/api/tasks", {task: text, deadline: deadline})
+    //         .then(res => {
+    //             tasks.value = res.data;
+    //         }).catch(err => {
+    //             console.log(err);
+    //         });
+    //         // await getTasks();
+    // }
+
+    // const task = {
+    //     task: text,
+    //     deadline: deadline,
+    // }
+    const postTask = async () => {
+        try {
+            const res = await fetch('http://localhost:3000/api/tasks', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+                body: JSON.stringify({
+                    task: text,
+                    deadline: deadline,
+                })
+            });
+            const data = await res.json();
+            tasks.value = data[1];
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    const deleteTasks = async () => {
-        await axios.delete("http://localhost:3000/api")
+    // const updateTaskStatus = async (id: number) => {
+    //     await axios.put(`http://localhost:3000/api/tasks/${id}`)
+    //         .then(res => {
+    //             tasks.value = res.data;
+    //         }).catch(err => {
+    //             console.log(err);
+    //         });
+    //         // await getTasks();
+    // } 
+    const updateTaskStatus = async (id: number) => {
+        try {
+            const res = await fetch(`http://localhost:3000/api/tasks/${id}`, {
+                method: 'PUT',
+            });
+            const data = await res.json();
+            tasks.value = data[1];
+            // console.log(data[])
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // const deleteTask = async (id: number) => {
+    //     await axios.delete(`http://localhost:3000/api/tasks/${id}`)
+    //         .then(res => {
+    //             tasks.value = res.data;
+    //         }).catch(err => {
+    //             console.log(err);
+    //         });
+    //         // await getTasks();
+    // }
+    const deleteTask = async (id: number) => {
+        try {
+            const res = await fetch(`http://localhost:3000/api/tasks/${id}`, {
+                method: 'DELETE'
+            });
+            const data = await res.json();
+            tasks.value = data[1];
+            console.log(data[1]);
+        } catch (error) {
+            console.log(error);
+        }
     }
 </script>
 
 <template>
     <h1>todoリスト</h1>
     <input v-model="text" type="text" placeholder="やるべきこと">
-    <input type="submit" value="追加" @click="postTasks">
-    <div v-for="(task, index) in tasks">
-        {{ index+1 }} : {{ task.task }}
+    <input v-model="deadline" type="date" name="deadline">
+    <input type="submit" value="追加" @click="postTask" key="postTasks">
+    <!-- <input type="submit" value="削除" @click=""> -->
+    <div v-for="(task, index) in tasks" :key="index">
+        <!-- <div id=""></div> -->
+        {{ task.id }} : {{ task.task }} {{ task.deadline }} 
+        <p v-if='!task.status'>未処理</p>
+        <p v-else>処理済</p>
+        <input type="submit" value="削除" @click="deleteTask(task.id)">
+        <!-- <input type="submit" value="削除" @click="openModal"> -->
+        <Modal v-show="showContent" @click="closeModal"/>
+        <input type="submit" value="編集する" >
+        <input type="submit" value="処理済にする" @click="updateTaskStatus(task.id)">
     </div>
-    <p>{{ text }}</p>
+    <!-- {{ deadline }} -->
 </template>
+
+<style scoped>
+    
+
+</style>
+
+<!-- 実装予定機能 -->
+<!-- タスク（内容、期限、ステータス）右側に処理済 編集 削除
+表示切替（全て、未処理、処理済み）
+フィルター（カテゴリー）
+並び替え（日付） -->
